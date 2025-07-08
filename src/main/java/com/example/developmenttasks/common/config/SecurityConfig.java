1268559af6719b01d57b5ac762f6d3d1e6227307
ansdudn2 <ansdudn2@naver.com>
2025-07-09 07:20:02 +0900

    feat:add securityconfig

--- src/main/java/com/example/developmenttasks/common/config/SecurityConfig.java
+++ src/main/java/com/example/developmenttasks/common/config/SecurityConfig.java
@@ -1,4 +1,39 @@
 package com.example.developmenttasks.common.config;
 
+import com.example.developmenttasks.common.security.JwtAuthenticationFilter;
+import com.example.developmenttasks.common.security.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain springSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.
+                csrf(csrf-> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authz->authz.requestMatchers("/login","/signup","/swagger-ui/**","/v3/api-docs/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
+                        UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
 }
