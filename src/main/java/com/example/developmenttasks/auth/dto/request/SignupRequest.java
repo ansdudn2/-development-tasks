94788bb8ad0d49de0f4fd17e4c7c11f1fae79d45
ansdudn2 <ansdudn2@naver.com>
2025-07-27 04:45:42 +0900

    feat: add testCode

--- src/main/java/com/example/developmenttasks/auth/dto/request/SignupRequest.java
+++ src/main/java/com/example/developmenttasks/auth/dto/request/SignupRequest.java
@@ -1,8 +1,10 @@
 package com.example.developmenttasks.auth.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class SignupRequest {
     private String username;
     private String password;
